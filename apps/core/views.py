94ac6d3116114ae8d3e35b61ee1a34fe4cbb3006
Croissant (Mohammad) <118067995+Javanitalab@users.noreply.github.com>
2024-04-30 19:10:33 +0200

    difference between ModelViewSet and ViewSet

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,34 @@
+import datetime
+
 from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, ViewSet
+
+from apps.core.models import History
+
 
 # Create your views here.
+
+
+class NumberOneViewSet(ModelViewSet):
+    queryset = History.objects.all()
+
+
+class NumberTwoViewSet(ViewSet):
+
+    @action(detail=False, methods=['post'])
+    def create(self, request, *args, **kwargs):
+        self.request.data
+        self.request.query_param
+        user_id = self.request.data.get('user', None)
+        description = self.request.data.get('description', None)
+        history = History.objects.create(user_id, description=description)
+
+        return Response(
+            status=201,
+            data={
+                'id': history.id,
+                'description': history.description,
+                'trigger_date': history.trigger_date.strftime('y%-m%-d-%'),
+            })
